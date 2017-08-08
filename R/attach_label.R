501e0f24029d6552501152d2dc3ee5ce428d68d8
jjchern <jiajia.chern@gmail.com>
2017-08-07 21:26:05 -0500

    Update docs

--- R/attach_label.R
+++ R/attach_label.R
@@ -2,7 +2,12 @@
 #' Attach variable labels
 #'
 #' @md
-#' @return A tibble, with proper variable labels
+#' @param df An extracted data frame from the scorecard merged files
+#' @return A data frame, with proper variable labels
+#' @examples
+#' \dontrun{
+#' attach_var_label(scorecard::mf2014_15)
+#' }
 #' @export
 attach_var_label = function(df){
   labelled::var_label(df) <- var_label_lst
@@ -12,7 +17,12 @@ attach_var_label = function(df){
 #' Attach value labels
 #'
 #' @md
-#' @return A tibble, with proper value labels
+#' @param df An extracted data frame from the scorecard merged files
+#' @return A data frame, with proper value labels
+#' @examples
+#' \dontrun{
+#' attach_var_label(scorecard::mf2014_15)
+#' }
 #' @export
 attach_val_label = function(df){
   labelled::val_labels(df) <- val_label_lst
